51c2e62db5a4c7048bff3cf121f710d848122bbb
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-02 14:15:14 +0200

    Fix matomo to support visistors w/o cookies  (#36)
    * tweak matomo
    
    * oops

--- themes/ropensci/static/scripts/matomo.js
+++ themes/ropensci/static/scripts/matomo.js
@@ -41,11 +41,11 @@
             var didConsent = this.hasConsented()
             if (type === 'opt-in' && didConsent) {
                 // enable cookies
-                _paq.push(['rememberConsentGiven']);
+                _paq.push(['rememberCookieConsentGiven']);
             }
             if (type == 'opt-out' && !didConsent) {
                 // disable cookies
-                _paq.push(['forgetConsentGiven']);
+                _paq.push(['forgetCookieConsentGiven']);
             }
         },
 
@@ -54,11 +54,11 @@
             var didConsent = this.hasConsented()
             if (type === 'opt-in' && didConsent) {
                 // enable cookies
-                _paq.push(['rememberConsentGiven']);
+                _paq.push(['rememberCookieConsentGiven']);
             }
             if (type == 'opt-out' && !didConsent) {
                 // disable cookies
-                _paq.push(['forgetConsentGiven'], 24);
+                _paq.push(['forgetCookieConsentGiven'], 24);
             }
         },
 
@@ -66,11 +66,11 @@
             var type = this.options.type
             if (type === 'opt-in') {
                 // disable cookies
-                _paq.push(['forgetConsentGiven']);
+                _paq.push(['forgetCookieConsentGiven']);
             }
             if (type == 'opt-out' && !didConsent) {
                 // disable cookies
-                _paq.push(['forgetConsentGiven'], 24);
+                _paq.push(['forgetCookieConsentGiven'], 24);
             }
         }
     })
