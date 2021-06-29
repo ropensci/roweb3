7b02688b3e9dd8c81bd63670f5a52948343f8f52
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-06-29 16:20:12 +0200

    better

--- themes/ropensci/static/scripts/search.js
+++ themes/ropensci/static/scripts/search.js
@@ -12,3 +12,72 @@
       });
     }
   })();
+  
+  
+(function () {
+ var fuse;
+  $("#search-input").focus(async function(e) {
+    if (fuse) {
+      return;
+    }
+
+    $(e.target).addClass("loading");
+    var response = await fetch($("#search-input").data("search-index"));
+    var data = await response.json();
+
+    var options = {
+      keys: ["title", "description", "content"],
+      ignoreLocation: true,
+      threshold: 0.1,
+      includeMatches: true,
+      includeScore: true,
+    };
+    fuse = new Fuse(data, options);
+
+    $(e.target).removeClass("loading");
+    console.log(fuse);
+  });
+
+  // Use algolia autocomplete
+  var options = {
+    autoselect: true,
+    debug: true,
+    hint: false,
+    minLength: 2,
+  };
+  var q;
+  
+  async function searchFuse(query, callback) {
+  await fuse;
+  var items;
+  if (!fuse) {
+    items = [];
+  } else {
+    q = query;
+    var results = fuse.search(query, { limit: 20 });
+    items = results
+      .filter((x) => x.score <= 0.75)
+      .map((x) => x.item);
+    if (items.length === 0) {
+      items = [{where:"Sorry 😿",what:"No results found.",path:window.location.href}];
+    }
+  }
+  callback(items);
+}
+
+autocomplete("#search-input", options, [
+    {
+      name: "content",
+      source: searchFuse,
+      templates: {
+        suggestion: (s) => {
+            return `${s.title} >	<div class="search-details"> ${s.description}</div>`;
+         
+        },
+      },
+    },
+  ]).on('autocomplete:selected', function(event, s) {
+    window.location.href = s.path + "?q=" + q + "#" + s.id;
+  });
+  
+  })();
