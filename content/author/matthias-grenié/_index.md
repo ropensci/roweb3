014e7b9bcc25ba9e40d5bf6d5f1448e4f04454ab
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2022-12-06 17:30:42 +0100

    Saving ggplot2 objects in a targets workflow technote (#439)
    * Add ORCiD in Author page
    
    * Add very rough draft blogpost
    
    * Structure blogpost a little more
    
    * Exlpain example dataset
    
    * start small workflow
    
    * start sentence on environment
    
    * Started fleshing out first two parts
    
    * Finish section on hooks
    
    * Remove part of the template
    
    * Finished 3rd & 4th sections + add resources
    
    * Add acknowledgments
    
    * Finalize post
    
    * Render post
    
    * Really render post
    
    * Update post with proper render & spellcheck
    
    * Update post with new hugo version
    
    * Apply suggestions from review
    
    Co-authored-by: Steffi LaZerte <steffi@steffi.ca>
    
    * Apply suggestions from review
    
    Co-authored-by: Steffi LaZerte <steffi@steffi.ca>
    
    * Apply suggestions from review
    
    Co-authored-by: Steffi LaZerte <steffi@steffi.ca>
    
    * Apply suggestions from review
    
    Co-authored-by: Steffi LaZerte <steffi@steffi.ca>
    
    * Apply suggestions from review
    
    Co-authored-by: Steffi LaZerte <steffi@steffi.ca>
    
    * Fix most problems revealed through review
    
    * Change folder date
    
    * Add alt text to all chunks that need it
    
    * Put all new sentences on new lines following style guide
    
    * Rerender blogpost
    
    * Apply suggestions from code review
    
    Co-authored-by: Steffi LaZerte <steffi@steffi.ca>
    
    * Update following comments and remove messages
    
    Co-authored-by: Steffi LaZerte <steffi@steffi.ca>

--- content/author/matthias-grenié/_index.md
+++ content/author/matthias-grenié/_index.md
@@ -3,4 +3,5 @@ name: Matthias Grenié
 link: https://rekyt.github.io/
 twitter: LeNematode
 github: Rekyt
+orcid: 0000-0002-4659-7522
 ---
---
+++ content/blog/2022-12-06-save-ggplot2-targets/_targets.R
@@ -0,0 +1,17 @@
+library(targets)
+library("ggplot2")
+library("targets")
+list(tar_target(mtcars_df, mtcars), tar_target(simple_plot, ggplot(mtcars_df, 
+    aes(cyl, mpg)) + geom_point()), tar_target(complex_plot, 
+    {
+        mtcars_df2 = mtcars_df
+        mtcars_df3 = rbind(mtcars_df, mtcars_df2)
+        mtcars_df4 = rbind(mtcars_df3, mtcars_df3)
+        ggplot(mtcars_df, aes(cyl, mpg)) + geom_point()
+    }), tar_target(complex_but_slim_plot, {
+    mtcars_df2 = mtcars_df
+    mtcars_df3 = rbind(mtcars_df, mtcars_df2)
+    mtcars_df4 = rbind(mtcars_df3, mtcars_df3)
+    rm(mtcars_df4, mtcars_df2, mtcars_df3)
+    ggplot(mtcars_df, aes(cyl, mpg)) + geom_point()
+}))
