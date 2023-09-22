bcdc03fcff092da26f3e25ef8928c171bda83868
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-09-22 16:05:03 +0200

    aah

--- themes/ropensci/static/scripts/copypaste.js
+++ themes/ropensci/static/scripts/copypaste.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('a.cite').forEach(el => {
   const btn = document.createElement('button');
   btn.typeName = 'button';
-  btn.className = 'btn btn-primary copy-button';
+  btn.className = 'btn-sm btn-primary copy-button';
   btn.innerHTML = "<i class='fa fa-copy'></i>";
   btn.onclick = () => navigator.clipboard.writeText(el.getAttribute("href"));
   el.parentNode.append(btn);
