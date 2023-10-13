26d7449c0badc1205cab5a8b0f6ca4885113b4ee
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-13 14:13:42 +0200

    Normalize link icon
    This reverts commit 6c63732f4cb9c33657fd8228fcc848120e500d9d.

--- themes/ropensci/static/scripts/copypaste.js
+++ themes/ropensci/static/scripts/copypaste.js
@@ -1,8 +1,6 @@
-document.querySelectorAll('a.cite').forEach(el => {
-  const btn = document.createElement('button');
-  btn.typeName = 'button';
-  btn.className = 'btn-sm btn-primary copy-button';
-  btn.innerHTML = "<i class='fa fa-copy'></i>";
-  btn.onclick = () => navigator.clipboard.writeText(el.getAttribute("href"));
-  el.parentNode.append(btn);
+$('.copy-cite-link').click(function(e){
+  e.preventDefault();
+  alert("Copying link to cliboard!");
+  var txt = $(this).parent().focus().attr('href');
+  navigator.clipboard.writeText(txt);
 });
