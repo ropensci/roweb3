0561a3a955d6f2b45437cbdff1abdc0aee584473
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-17 12:26:05 +0200

    tweak copy doi

--- themes/ropensci/static/scripts/copypaste.js
+++ themes/ropensci/static/scripts/copypaste.js
@@ -1,6 +1,13 @@
-$('.copy-cite-link').click(function(e){
-  e.preventDefault();
-  alert("Copying link to cliboard!");
-  var txt = $(this).parent().focus().attr('href');
-  navigator.clipboard.writeText(txt);
+$(function(){
+  var link = $('.copy-cite-link');
+  var tooltip_text = 'Copy DOI to clipboard';
+  $('.copy-cite-link').click(function(e){
+    var txt = link.parent().attr('href');
+    navigator.clipboard.writeText(txt).then(function(e){
+      link.attr('data-original-title', 'Copied!').tooltip('show');
+      link.attr('data-original-title', tooltip_text);
+    });
+    return false;
+  });
+  link.tooltip({title: tooltip_text});
 });
