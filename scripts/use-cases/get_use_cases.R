6e970f6cf57d197c194b0a9146e0eaa0ecf04826
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-05-05 14:39:20 +0200

    tweaks

--- scripts/use-cases/get_use_cases.R
+++ scripts/use-cases/get_use_cases.R
@@ -1,16 +1,104 @@
+old_use_cases <- jsonlite::read_json(
+  file.path("data", "usecases", "usecases-legacy.json")
+)
 use_cases_path <- file.path("data", "usecases", "usecases.json")
-use_cases <- jsonlite::read_json(use_cases_path)
 
-add_lang <- function(use_case) {
-  use_case[["language"]] <- cld3::detect_language(use_case[["title"]])
-  use_case
+
+discussions <- gh::gh(
+  "GET /repos/ropensci/discussions/discussions",
+  .limit = Inf
+)
+
+handle_discussion <- function(discussion) {
+  body <- discussion[["body"]] |>
+    commonmark::markdown_xml() |>
+    xml2::read_xml() |>
+    xml2::xml_ns_strip()
+
+  resource <- xml2::xml_find_first(
+    body,
+    "//heading[contains(., 'resource') or contains(., 'recurso')]/following-sibling::paragraph"
+  ) |>
+    xml2::xml_text()
+
+  url <- xml2::xml_find_first(
+    body,
+    "//heading[contains(., 'URL')]/following-sibling::paragraph"
+  ) |>
+    xml2::xml_text()
+
+  image <- xml2::xml_find_first(
+    body,
+    "//heading[contains(., 'Image') or contains(., 'Imagen')]/following-sibling::paragraph"
+  ) |>
+    xml2::xml_child() |>
+    xml2::xml_attr("destination")
+  image_path <- file.path(
+    "themes",
+    "ropensci",
+    "assets",
+    "usecases",
+    sprintf("use-case-%s", discussion$number)
+  )
+
+  if (!is.na(image)) {
+    curl::curl_download(
+      image,
+      image_path
+    )
+  }
+  image_info <- magick::image_read(image_path) |>
+    magick::image_info()
+  image_ext <- tolower(image_info[1, ][["format"]])
+  new_image_path <- fs::path_ext_set(image_path, sprintf(".%s", image_ext))
+  fs::file_move(
+    image_path,
+    new_image_path
+  )
+
+  alt <- xml2::xml_find_first(
+    body,
+    "//heading[contains(., 'Alt') or contains(., 'alternativo')]/following-sibling::paragraph"
+  ) |>
+    xml2::xml_text()
+
+  language <- xml2::xml_find_first(
+    body,
+    "//heading[contains(., 'Language') or contains(., 'Idioma')]/following-sibling::paragraph"
+  ) |>
+    xml2::xml_text()
+
+  language <- sub("Español", "Spanish", language)
+  language <- sub("Inglés", "English", language)
+
+  title <- discussion[["title"]]
+
+  reporter_name <- gh::gh(
+    "GET /users/{user}",
+    user = discussion$user$login
+  )$name
+  reporter <- sprintf("[%s](%s)", reporter_name, discussion$user$url)
+
+  date <- as.Date(discussion$created_at)
+
+  list(
+    title = title,
+    reporter = reporter,
+    resource = resource,
+    url = url,
+    image = fs::path_file(new_image_path),
+    alt = alt,
+    date = date,
+    language = language
+  )
 }
 
-use_cases <- purrr::map(use_cases, add_lang)
+use_cases <- purrr::map(discussions, handle_discussion)
+use_cases <- c(old_use_cases, use_cases)
 
 jsonlite::write_json(
   use_cases,
   use_cases_path,
-  pretty = TRUE,
-  auto_unbox = TRUE
+  auto_unbox = TRUE,
+  pretty = TRUE
 )
